36de883e075a61ea2607a259686a165ab64ff3ef
Eugene Kwan <ekwan16@gmail.com>
2020-06-07 19:36:35 -0400

    working on multi-molecule training

---
+++ training/small/download.py
@@ -0,0 +1,3 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+gdd.download_file_from_google_drive(file_id='1VkZouJFv_h3nlzPlOfBZ1c9IW2RYe8i', dest_path='small.zip', unzip=True, showsize=True)
