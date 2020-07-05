114fab099e92946f405f46f4bbe846f6a81fd34e
Eugene Kwan <ekwan16@gmail.com>
2020-07-05 15:34:28 -0400

    full qm7 training

--- training/qm7/training_utils.py
+++ training/qm7/training_utils.py
@@ -7,6 +7,38 @@ import torch_geometric as tg
 import math
 from time import perf_counter
 
+class Stopwatch():
+    def __init__(self, running=False):
+        self.elapsed = 0
+        self.start_time = None
+        self.running = False
+        if running:
+            self.start()
+            
+    def start(self):
+        assert not self.running, "can't start a stopwatch that's already started"
+        self.start_time = time.time()
+        self.running = True
+        
+    def stop(self):
+        assert self.running, "can't stop a stopwatch that isn't running"
+        now = time.time()
+        self.elapsed += now - self.start_time
+        self.start_time = None
+        self.running = False
+        
+    def get_elapsed(self):
+        if self.running:
+            now = time.time()
+            elapsed = self.elapsed + now - self.start_time
+            return elapsed
+        else:
+            return self.elapsed
+    
+    def reset(self):
+        self.elapsed = 0
+        self.running = False
+        self.start_time = None
 
 def loss_fn_mae(x, y):
     return (x - y).abs().mean()
